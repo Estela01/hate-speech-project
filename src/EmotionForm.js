d3311908296886b80eec178c66cb2e141974db37
SchafferCPqD <schaffer@cpqd.com.br>
2019-11-22 11:07:59 -0300

    Remove unused imports

--- src/EmotionForm.js
+++ src/EmotionForm.js
@@ -9,15 +9,8 @@ import LockOutlinedIcon from '@material-ui/icons/ModeComment';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
-import StarIcon from '@material-ui/icons/StarBorder';
 import Toolbar from '@material-ui/core/Toolbar';
 import Link from '@material-ui/core/Link';
-import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
 import hate from './backgroundfoto.jpeg';
 
 
